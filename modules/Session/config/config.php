27396fdfa77a91af30101b99060187420038ddd4
partikule <studio@partikule.net>
2012-01-10 11:11:11 +0100

    Session module : config.php

--- modules/Session/config/config.php
+++ modules/Session/config/config.php
@@ -9,7 +9,6 @@ $config['module_session_name'] = "Session";
 |--------------------------------------------------------------------------
 | Coma separated value of session indexes which can be modified by
 | the module.
-| These values are set through the Ionize Admin panel.
 |
 */
 $config['module_session_allowed_variables'] = 'ma_var';
