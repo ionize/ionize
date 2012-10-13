d9ed8da60a2a57213d1c8a7dd9fc07edd7cf32f3
Iskender <iskender@6ve1.com>
2012-10-13 11:50:38 +0300

    Theme -> Default tags changed with current version

--- themes/default/views/page.php
+++ themes/default/views/page.php
@@ -1,18 +1,48 @@
-<ion:partial path="header" />
+<ion:partial view="header" />
 
-  <div class="span-24">
-  
-    <!-- Page title -->
-    <ion:title tag="h2" />
-    
-    
-    <!-- Articles -->
-    <ion:articles>
-          
-      <ion:article />
-      
-    </ion:articles>  
-  
-  </div>
+    <div class="span-24">
 
-<ion:partial path="footer" />
+        <!-- Page title -->
+        <ion:title tag="h2" />
+
+        <!-- Current Page -->
+        <ion:page>
+
+            <!-- Articles -->
+            <ion:articles>
+
+                <!-- Article -->
+                <ion:article>
+
+                    <div class="article">
+
+                        <ion:title tag="h3" />
+
+                        <!-- Formatted date -->
+
+
+                        <!-- Article content -->
+                        <ion:content />
+
+                        <div class="article-pictures">
+
+                            <!-- Articles linked pictures -->
+                            <ion:medias type="picture">
+
+                                <img src="<ion:src />" />
+
+                            </ion:medias>
+
+                        </div>
+
+                    </div>
+
+                </ion:article>
+
+            </ion:articles>
+
+        </ion:page>
+
+    </div>
+
+<ion:partial view="footer" />
