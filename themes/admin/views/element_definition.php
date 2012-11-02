87a83fba686e5bd7ca17172f232dae61cf4e8658
Iskender <iskender@6ve1.com>
2012-11-02 14:53:01 +0200

    Element Definition Accordion List Style changed with old style
    Drag & Drop not working for accordion style

--- themes/admin/views/element_definition.php
+++ themes/admin/views/element_definition.php
@@ -1,126 +1,121 @@
 <?php
 
-    $id = $id_element_definition;
+$id = $id_element_definition;
 
 ?>
-<h3 class="toggler toggler-<?php echo $id; ?>"><?php echo $name; ?></h3>
-    <div class="element element-<?php echo $id; ?>">
-        <li class="sortme nohover element_definition" id="element_definition_--><?//= $id; ?><!--" rel="--><?//= $id; ?><!--">
-	
-	
-	<div class="h20">
 
-		<a class="icon delete right" rel="<?php echo $id; ?>"></a>
-
-		<a class="icon drag left mr10"></a>
-
-<!--	<span class="toggler left" style="display:block;height:16px;" rel="<?php echo $id; ?>"></span>-->
+<li class="sortme nohover element_definition" id="element_definition_<?= $id ?>" rel="<?= $id ?>">
 
 
-		<?php if ($name == '') :?>
-			<input id="elementName<?php echo $id; ?>" type="text" class="inputtext w120 left" />
-			
-				<a id="elementDefinitionSave<?php echo $id; ?>" class="light button left ml10">
-					<i class="icon-save"></i><?php echo lang('ionize_label_element_set_name'); ?>
-				</a>
-			
-			<script type="text/javascript">
-			
-				$('elementDefinitionSave<?php echo $id; ?>').addEvent('click', function()
-				{
-					ION.sendData('element_definition/save_field', {'id':'<?php echo $id; ?>', 'field': 'name', 'value': $('elementName<?php echo $id; ?>').value, selector:'.element_definition a.name[rel=<?php echo $id; ?>]'});
-				});
-				
-				$('elementName<?php echo $id; ?>').focus();
-				$('element_definition_<?php echo $id; ?>').addClass('stripped');
-			
-			</script>
-			
-		<?php else :?>
-			<a class="edit name left" rel="<?php echo $id; ?>"><?php echo $name; ?></a>
-		<?php endif ;?>
-	</div>
+    <div class="h20">
 
-	<?php if ($name != '') :?>
+        <a class="icon delete right" rel="<?= $id ?>"></a>
 
-	<div class="clear mt10 mr20 ml40 ">
-	
-		<!-- Tabs -->
-		<div id="elementDefinitonTab<?php echo $id; ?>" class="mainTabs transparent">
-			
-			<ul class="tab-menu">
-				<?php foreach(Settings::get_languages() as $language) :?>
-					<li <?php if($language['def'] == '1') :?> class="dl"<?php endif ;?>><a><?php echo ucfirst($language['name']); ?></a></li>
-				<?php endforeach ;?>
-			</ul>
-			<div class="clear"></div>
-		
-		</div>
-	
-		<div id="elementDefinitonTabContent<?php echo $id; ?>" class="ml20 mr20 mb10">
-	
-			<!-- Text block -->
-			<?php foreach(Settings::get_languages() as $language) :?>
-		
-				<?php 
-				
-					$lang = $language['lang']; 
-					$aTitle = lang('ionize_label_change').' '.lang('ionize_label_title');
-				
-				?>
-				
-				<div class="tabcontent">
-					<a class="edit title left" rel="<?php echo $id; ?>.<?php echo $lang; ?>" title="<?php echo $aTitle; ?>"><?php echo ${$lang}['title']; ?></a>
-				</div>
-	
-			<?php endforeach ;?>
-		
-		
-		</div>
-		
-		<hr />
-	
-		<!-- Fields -->
-		<div style="overflow:hidden;clear:both;">
-			
-			<div class="pt5" id="def_<?php echo $id; ?>">
-				
-				<!-- Add Field button -->
-				<?php if ($id != 0) :?>
-					<a class="light button mb5 ml5 add_field" rel="<?php echo $id; ?>">
-						<i class="icon-plus"></i><?php echo lang('ionize_label_element_add_field'); ?>
-					</a>
-				<?php endif ;?>
-				
-				<ul class="fields" id="fields<?php echo $id; ?>" rel="<?php echo $id; ?>">
-	
-					<?php foreach($fields as $field) :?>
-						<li class="sortme" rel="<?php echo $field['id_extend_field']; ?>">
-							<a class="icon delete right" rel="<?php echo $field['id_extend_field']; ?>"></a>
+        <a class="icon drag left mr10"></a>
+
+        <!--	<span class="toggler left" style="display:block;height:16px;" rel="<?= $id ?>"></span>-->
+
+
+        <?php if ($name == '') :?>
+        <input id="elementName<?= $id ?>" type="text" class="inputtext w120 left" />
+
+        <button id="elementDefinitionSave<?= $id ?>" type="button" class="light-button left ml10" value="Save"><?= lang('ionize_label_element_set_name') ?></button>
+
+        <script type="text/javascript">
+
+            $('elementDefinitionSave<?= $id ?>').addEvent('click', function()
+            {
+                ION.sendData('element_definition/save_field', {'id':'<?= $id ?>', 'field': 'name', 'value': $('elementName<?= $id ?>').value, selector:'.element_definition a.name[rel=<?= $id ?>]'});
+            });
+
+            $('elementName<?= $id ?>').focus();
+            $('element_definition_<?= $id ?>').addClass('stripped');
+
+        </script>
+
+        <?php else :?>
+        <a class="edit name left" rel="<?= $id ?>"><?= $name ?></a>
+        <?php endif ;?>
+    </div>
+
+    <?php if ($name != '') :?>
+
+    <div class="clear mt10 mr20 ml40 ">
+
+        <!-- Tabs -->
+        <div id="elementDefinitonTab<?= $id ?>" class="mainTabs transparent">
+
+            <ul class="tab-menu">
+                <?php foreach(Settings::get_languages() as $language) :?>
+                <li <?php if($language['def'] == '1') :?> class="dl"<?php endif ;?>><a><?= ucfirst($language['name']) ?></a></li>
+                <?php endforeach ;?>
+            </ul>
+            <div class="clear"></div>
+
+        </div>
+
+        <div id="elementDefinitonTabContent<?= $id ?>" class="ml20 mr20 mb10">
+
+            <!-- Text block -->
+            <?php foreach(Settings::get_languages() as $language) :?>
+
+            <?php
+
+            $lang = $language['lang'];
+            $aTitle = lang('ionize_label_change').' '.lang('ionize_label_title');
+
+            ?>
+
+            <div class="tabcontent">
+                <a class="edit title left" rel="<?= $id ?>.<?= $lang ?>" title="<?= $aTitle ?>"><?= ${$lang}['title'] ?></a>
+            </div>
+
+            <?php endforeach ;?>
+
+
+        </div>
+
+        <hr />
+
+        <!-- Fields -->
+        <div style="overflow:hidden;clear:both;">
+
+            <div class="pt5" id="def_<?= $id ?>">
+
+                <!-- Add Field button -->
+                <?php if ($id != 0) :?>
+                <input type="button" class="light-button plus mb5 ml5 add_field" value="Add field" rel="<?= $id ?>"></input>
+                <?php endif ;?>
+
+                <ul class="fields" id="fields<?= $id ?>" rel="<?= $id ?>">
+
+                    <?php foreach($fields as $field) :?>
+                    <li class="sortme" rel="<?= $field['id_extend_field'] ?>">
+                        <a class="icon delete right" rel="<?= $field['id_extend_field'] ?>"></a>
 							
-							<span class="lite right mr10" rel="<?php echo $field['id_extend_field']; ?>">
-								<?php echo $field['type_name']; ?>
-								<?php if($field['translated'] == '1') :?>
-									 / <?php echo lang('ionize_label_multilingual'); ?>
-								<?php endif ;?>
+							<span class="lite right mr10" rel="<?= $field['id_extend_field'] ?>">
+								<?= $field['type_name'] ?>
+                                <?php if($field['translated'] == '1') :?>
+                                / <?= lang('ionize_label_multilingual') ?>
+                                <?php endif ;?>
 
 							</span>
-							
-							<a class="icon drag left"></a>
-							<a class="left ml10 edit_field" rel="<?php echo $field['id_extend_field']; ?>"><?php echo $field['name']; ?></a>
-						</li>
-					<?php endforeach ;?>
-				</ul>
-			</div>
-		</div>
-	
-	
-	</div>
-	
-	<?php endif ;?>
-    </li>
-</div>
+
+                        <a class="icon drag left"></a>
+                        <a class="left ml10 edit_field" rel="<?= $field['id_extend_field'] ?>"><?= $field['name'] ?></a>
+                    </li>
+                    <?php endforeach ;?>
+                </ul>
+            </div>
+        </div>
+
+
+    </div>
+
+    <?php endif ;?>
+
+
+</li>
 <script type="text/javascript">
-	new TabSwapper({tabsContainer: 'elementDefinitonTab<?php echo $id; ?>', sectionsContainer: 'elementDefinitonTabContent<?php echo $id; ?>', selectedClass: 'selected', deselectedClass: '', tabs: 'li', clickers: 'li a', sections: 'div.tabcontent'});
-    ION.initAccordion('.toggler-<?php echo $id; ?>', 'div.element-<?php echo $id; ?>', true, 'elementDefinationAccordion-<?php echo $id; ?>');
+    new TabSwapper({tabsContainer: 'elementDefinitonTab<?= $id ?>', sectionsContainer: 'elementDefinitonTabContent<?= $id ?>', selectedClass: 'selected', deselectedClass: '', tabs: 'li', clickers: 'li a', sections: 'div.tabcontent'});
 </script>
