a9972e84b7f831951bb177602e46a4e18b082a9a
Iskender <iskender@6ve1.com>
2012-12-03 00:54:38 +0200

    Search Module Turkish translation update

--- modules/Search/language/tr/search_lang.php
+++ modules/Search/language/tr/search_lang.php
@@ -5,7 +5,7 @@
 | Search module Language file
 |
 | Language : 	English
-| Tranlsator : 	Ukyo, 15th 09 2010
+| Tranlsator : 	Ukyo, 05th 09 2012
 |
 | Notes : Modules translation items should begin with the prefix 'module_name'
 |		  where 'module_name' is the name of the module
@@ -13,8 +13,13 @@
 |--------------------------------------------------------------------------
 */
 
-$lang['module_search_button_start'] =			'Aramayı başlat';
-$lang['module_search_message_no_results'] =		'Sonuç bulunamadı';
-$lang['module_search_fill_the_field'] =			'Arama Terimleriniz :';
-$lang['module_search_results_title'] =			'Sonuçlar';
+$lang['module_search_input_label'] =		'Ara';
+$lang['module_search_button_start'] =		'Aramayı başlat';
+$lang['module_search_message_no_results'] =	'Sonuç bulunamadı';
+$lang['module_search_fill_the_field'] =		'Aradığınız öğe :';
+$lang['module_search_results_title'] =		'Arama sonucu';
+$lang['module_search_form'] =				'Ara...';
+
+$lang['module_search_result_in'] =				'İçindeki sonuç : ';
+$lang['module_search_read_complete_article'] =	'Makaleyi oku...';
 
