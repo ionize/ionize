b361de1f3599e950593a891d56a43c3ca99c44e6
Michel-Ange <studio@partikule.net>
2012-12-20 15:44:14 +0100

    Remove "tags" theme from master.

--- themes/tags/language/fr/tags_lang.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$lang['read_more'] = "Lire l'article";
-$lang['next_article'] = "Article suivant :";
-$lang['previous_article'] = "Article précédent : ";
-$lang['you_are_here'] = "Vous êtes là : ";
-
-?>
