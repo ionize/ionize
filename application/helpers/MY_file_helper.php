733ae4019e0531647127961e1c843847968d5f92
Michel-Ange <studio@partikule.net>
2014-02-25 17:54:35 +0000

    glob check

--- application/helpers/MY_file_helper.php
+++ application/helpers/MY_file_helper.php
@@ -19,12 +19,17 @@ if ( ! function_exists('glob_recursive'))
 	function glob_recursive($pattern, $flags = 0)
 	{
 		$files = glob($pattern, $flags);
+		$dirs = glob(dirname($pattern).'/*', GLOB_ONLYDIR|GLOB_NOSORT);
 
-		foreach (glob(dirname($pattern).'/*', GLOB_ONLYDIR|GLOB_NOSORT) as $dir)
-		{
-			$files = array_merge($files, glob_recursive($dir.'/'.basename($pattern), $flags));
+		if( is_array($files) && ! empty($dirs) ) {
+
+			foreach ($dirs as $dir)
+			{
+				$_files = glob_recursive($dir.'/'.basename($pattern), $flags);
+				if(is_array($_files))
+					$files = array_merge($files, $_files);
+			}
 		}
-
 		return $files;
 	}
 }
