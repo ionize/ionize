02ce544ec7dae7ab78deb31775bd1a3a570d23b1
Kay Stenschke <info@stenschke.com>
2015-07-25 14:21:58 +0200

    added to file helper: remove_directory()

--- application/helpers/MY_file_helper.php
+++ application/helpers/MY_file_helper.php
@@ -46,3 +46,44 @@ if ( ! function_exists('mb_pathinfo'))
 		return $ret;
 	}
 }
+
+if ( ! function_exists('remove_directory'))
+{
+	function remove_directory($directory, $empty=FALSE)
+	{
+		if(substr($directory,-1) == '/') {
+			$directory = substr($directory,0,-1);
+		}
+
+		if(!file_exists($directory) || !is_dir($directory)) {
+			return FALSE;
+		} elseif(!is_readable($directory)) {
+
+			return FALSE;
+
+		} else {
+
+			$handle = opendir($directory);
+			while (FALSE !== ($item = readdir($handle)))
+			{
+				if($item != '.' && $item != '..') {
+					$path = $directory.'/'.$item;
+					if(is_dir($path)) {
+						remove_directory($path);
+					}else{
+						unlink($path);
+					}
+				}
+			}
+			closedir($handle);
+			if($empty == FALSE)
+			{
+				if(!rmdir($directory))
+				{
+					return FALSE;
+				}
+			}
+			return TRUE;
+		}
+	}
+}
