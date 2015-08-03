ionize/ionize

commit 7c9735f5f086fc5284a17307b0a18110e491dcf7

ionize JS cleanup: reduced unnecessary temp. variables, added omitted semicolons (browser compatibility), removed unnecessary semicolons, reduced duplicate variable definitions, reduced repeated identical sizzle selectors, removed silly (self referential) assignments, corrected some doc comments, simplified some conditions via ternary operation, simplified array instanciations, corrected some typos, made parseInt() calls failsave (added base)

themes/admin/javascript/ionize/ionize_authority.js
@@ -18,7 +18,7 @@ ION.Authority = new Class({
 	/**
 	 * All rules, by type
 	 */
-	all_rules: Array(),
+	all_rules: [],
 
 	has_all: false,
 
@@ -105,8 +105,7 @@ ION.Authority = new Class({
 	 *
 	 * @param action
 	 * @param resource
-	 * @param check_has_rule
-	 * @returns {boolean}
+	 * @returns {Boolean}
 	 *
 	 */
 	can:function(action, resource)
@@ -139,16 +138,14 @@ ION.Authority = new Class({
 
 	resourceHasRule: function(resource)
 	{
-		var has = false;
-
 		Object.each(ION.Authority.all_rules, function(rule)
 		{
 			if (resource == rule.resource)
 			{
-				has = true;
+				return true;
 			}
 		});
 
-		return has;
+		return false;
 	}
 });

themes/admin/javascript/ionize/ionize_button.js
@@ -68,10 +68,7 @@ ION.ButtonToolbar = new Class({
 	 */
 	hasButton: function(id)
 	{
-		if (this.getButtonById(id) != null)
-			return true;
-
-		return false;
+		return (this.getButtonById(id) != null);
 	},
 
 	adopt:function(element)
@@ -215,7 +212,7 @@ ION.Button = new Class({
 					self.btnGroup.addClass('open');
 					self._correctBtnGroupPosition();
 				}
-			}
+			};
 
 			this.btnGroup.addEvent('click', this.options.onClick);
 
@@ -320,7 +317,7 @@ ION.Button = new Class({
 	 * Activates one button
 	 * (make it selected)
 	 *
-	 * @param args		String or Array of IDs. Partners to unactivate
+	 * @param args		String or Array of IDs. Partners to deactivate
 	 */
 	activate: function()
 	{

themes/admin/javascript/ionize/ionize_content.js
@@ -12,11 +12,12 @@ ION.append({
 
 		if (typeOf(user) != 'null')
 		{
-			$('mainPanel').removeClass('bg-gray');
+			var elMainPanel = $('mainPanel');
+			elMainPanel.removeClass('bg-gray');
 			if (ION.grayPanels.contains(options.url) == true)
 				$('mainPanel').addClass('bg-gray');
 
-			$('mainPanel').getElements('iframe').each(function(el){el.destroy()});
+			elMainPanel.getElements('iframe').each(function(el){el.destroy()});
 
 			options.method = 'post';
 			options.url = ION.adminUrl + ION.cleanUrl(options.url);
@@ -41,7 +42,8 @@ ION.append({
 	initToolbox: function(toolbox_url, onContentLoaded, data)
 	{
 		// Creates the header toolbox if it doesn't exists
-		if ( ! $('mainPanel_headerToolbox')) {
+		var elMainPanelHeaderToolbox = $('mainPanel_headerToolbox');
+		if ( ! elMainPanelHeaderToolbox) {
 			this.panelHeaderToolboxEl = new Element('div', {
 				'id': 'mainPanel_headerToolbox',
 				'class': 'buttonbar'
@@ -64,7 +66,7 @@ ION.append({
 		}
 		else
 		{
-			$('mainPanel_headerToolbox').empty();
+			elMainPanelHeaderToolbox.empty();
 
 			if (typeOf(onContentLoaded) == 'function')
 				onContentLoaded($('mainPanel_headerToolbox'));
@@ -111,9 +113,9 @@ ION.append({
 	 */
 	initModuleToolbox: function(module, toolbox_url)
 	{
-
 		// Creates the header toolbox if it doesn't exists
-		if ( ! $('mainPanel_headerToolbox')) {
+		var elMainPanelHeaderToolbox = $('mainPanel_headerToolbox');
+		if ( ! elMainPanelHeaderToolbox) {
 			this.panelHeaderToolboxEl = new Element('div', {
 				'id': 'mainPanel_headerToolbox',
 				'class': 'panel-header-toolbox'
@@ -129,7 +131,7 @@ ION.append({
 		}
 		else
 		{
-			$('mainPanel_headerToolbox').empty();
+			elMainPanelHeaderToolbox.empty();
 		}
 	
 	},
@@ -215,7 +217,7 @@ ION.append({
 		if (cookieName)
 			disp = [Cookie.read(cookieName), disp].pick();
 			
-		var acc = new Fx.Accordion(togglers, elements, {
+		return new Fx.Accordion(togglers, elements, {
 			display: disp,
 			opacity: false,
 			alwaysHide: true,
@@ -230,8 +232,6 @@ ION.append({
 			},
 			duration:'short'
 		});
-		
-		return acc;
 	},
 
 	
@@ -333,7 +333,7 @@ ION.append({
 	/**
 	 * Updates multiple elements
 	 *
-	 * @param	array	Array of elements to update. Array('element_id' => 'url_to_call')
+	 * @param	{Array}		elements	Array of elements to update. Array('element_id' => 'url_to_call')
 	 *
 	 */
 	updateElements: function (elements)
@@ -358,9 +358,7 @@ ION.append({
 	 * Updates one / several DOM elements
 	 * based on one HTML Request result
 	 *
-	 * @param	Object	Options
-	 *					'url' : URL of the controller to call
-	 *					'element' : ID, selectors of the DOM element(s) to update
+	 * @param	{Object}	options		'url' : URL of the controller to call, 'element' : ID, selectors of the DOM element(s) to update
 	 *
 	 */
 	updateElement: function (options)
@@ -438,7 +436,8 @@ ION.append({
 	 * Originally using the Monkey Physics Datepicker.
 	 * Currently using this one : abidibo / mootools-datepicker
 	 *
-	 * @param	String		PHP Date format
+	 * @param	{String}	dateFormat		PHP Date format
+	 * @param	{Object}	options
 	 *
 	 */
 	initDatepicker: function(dateFormat, options)
@@ -506,15 +505,16 @@ ION.append({
 		{
 			item.addEvent('click', function(e)
 			{
+				var div;
 				if (document.selection)
 				{
-					var div = document.body.createTextRange();
+					div = document.body.createTextRange();
 					div.moveToElementText(item);
 					div.select();
 				}
 				else
 				{
-					var div = document.createRange();
+					div = document.createRange();
 					div.setStartBefore(item);
 					div.setEndAfter(item) ;
 					window.getSelection().addRange(div);
@@ -578,9 +578,10 @@ ION.append({
 	
 	/**
 	 * Init the dynamic title update
-	 * @param	string	Input source ID
-	 * @param	string	HTML Dom Element destination ID
-	 * @param	string	Lang code
+	 *
+	 * @param	{String}	source		Input source ID
+	 * @param	{String}	dest 		HTML Dom Element destination ID
+	 * @param	{String}	lang 		Lang code
 	 *
 	 */
 	initTitleUpdate: function(source, dest, lang)
@@ -617,8 +618,12 @@ ION.append({
 	{
 		if (src && dest)
 		{
-			if (typeOf(src) == 'string') {src = $(src)};
-			if (typeOf(dest) == 'string') {dest = $(dest)};
+			if (typeOf(src) == 'string') {
+				src = $(src);
+			}
+			if (typeOf(dest) == 'string') {
+				dest = $(dest);
+			}
 			
 			dest.set('html', src.value);
 		}
@@ -643,9 +648,9 @@ ION.append({
 	/**
 	 * Adds drag'n'drop functionnality to one element
 	 *
-	 * @param	HTML Dom Element	Element to add the drag on.
-	 * @param	String				CSS classes which can drop the element, comma separated names.
-	 * @param	String				Callback function(s), comma separated names.
+	 * @param	{Element} 	el 				Dom Element	Element to add the drag on.
+	 * @param	{String}	droppables		CSS classes which can drop the element, comma separated names.
+	 * @param	{String}	dropCallbacks	Callback function(s), comma separated names.
 	 *
 	 * @usage
 	 * 			ION.addDragDrop (item, '.dropcreators', 
@@ -837,10 +842,13 @@ ION.append({
 			});
 		});
 	},
-	
+
+	/**
+	 * @param	{String}	selector	element or sizzle path
+	 */
 	initClearField: function(selector)
 	{
-		var selector = typeOf(selector) == 'string' ? $(selector) : selector;
+		selector = typeOf(selector) == 'string' ? $(selector) : selector;
 
 		if (selector)
 		{
@@ -902,6 +910,7 @@ ION.append({
 		    relative: true,
 			selectMode : options.selectMode ? options.selectMode : false,
 			width: typeOf(options.width) != 'null' ? options.width : 'inherit',
+
 		    'injectChoice': function(choice)
 		    {
 				// choice is one <li> element
@@ -919,6 +928,7 @@ ION.append({
 				// add the mouse events to the <li> element
 				this.addChoiceEvents(choice);
 			},
+
 			onSelection: function(selection, item, value, input)
 			{
 				var id = item.getProperty('data-id');
@@ -1012,7 +1022,8 @@ ION.append({
 	/**
 	 * Adds the number of medias to the corresponding tab
 	 *
-	 *
+	 * @param	{String}	id_tab
+	 * @param	{String}	id_container
 	 */
 	updateTabNumber: function(id_tab, id_container)
 	{
@@ -1025,16 +1036,11 @@ ION.append({
 			if (tab.getElement('span')) tab.getElement('span').dispose();
 			
 			var ul = $(id_container).getElement('ul');
-			
-			if (typeOf(ul) != 'null')
-			{
-				nb = (ul.getChildren('li')).length;		
-			}
-			else
-			{
-				nb = ($(id_container).getChildren()).length;
-			}
-			
+
+			nb = (typeOf(ul) != 'null')
+				? (ul.getChildren('li')).length
+				: ($(id_container).getChildren()).length;
+
 			if (nb > 0)
 			{
 				tab.adopt(new Element('span', {'class':'tab-detail'}).set('html',nb));
@@ -1048,6 +1054,7 @@ ION.append({
 	 * Called after a delete by the controller if the element definition
 	 * has no more element for this parent.
 	 *
+	 * @param	{String}	id
 	 */
 	deleteTab: function(id)
 	{
@@ -1274,15 +1281,16 @@ ION.append({
 		
 		for (var i=0; i< order.length; i++)
 		{
+			var el;
 			if (articleContainer)
 			{
-				var el = articleContainer.getElement('li.article' + args.id_page + 'x' + order[i]);
+				el = articleContainer.getElement('li.article' + args.id_page + 'x' + order[i]);
 				el.inject(articleContainer, 'top');
 			}
 			
 			if (typeOf(articleList) != 'null')
 			{
-				var el = articleList.getElement('li.article' + args.id_page + 'x' + order[i]);
+				el = articleList.getElement('li.article' + args.id_page + 'x' + order[i]);
 				el.inject(articleList, 'top');
 			}
 		}
@@ -1291,10 +1299,11 @@ ION.append({
 	
 	/**
 	 * Links one article to a page
-	 * @param	int		ID of the article
-	 * @param	int		ID of the page to add as parent
-	 * @param	int		ID of the original page. 0 if no one.
-	 * @param	Event	Event object.
+	 *
+	 * @param	{Number}	id_article		ID of the article
+	 * @param	{Number}	id_page			ID of the page to add as parent
+	 * @param	{Number}	id_page_origin	ID of the original page. 0 if no one.
+	 * @param	{Event}		event			Event object.
 	 *
 	 */
 	linkArticleToPage: function(id_article, id_page, id_page_origin, event)
@@ -1356,10 +1365,12 @@ ION.append({
 	 * Reloads the page's articles list
 	 * called by ION.linkArticleToPage()
 	 *
+	 * @param	{String}	id_page
 	 */
 	reloadPageArticleList: function(id_page)
 	{
-		if (typeOf($('id_page')) != 'null' && $('id_page').value == id_page)
+		var elIdPage = $('id_page');
+		if (typeOf(elIdPage) != 'null' && elIdPage.value == id_page)
 		{
 			ION.HTML(ION.adminUrl + 'article/get_list', {'id_page':id_page}, {'update': 'articleListContainer'});
 		}
@@ -1444,11 +1455,10 @@ ION.append({
 	 * Link an Element to a parent
 	 * Called by ION.dropContentElementInPage(), ION.dropContentElementInArticle()
 	 *
-	 * @param	String			Parent type. Can be 'article', 'page', etc.
-	 * @param	DOM Element		The copied / moved element
-	 * @param	DOM Element		DOM target
-	 * @param	Event			Event
-	 *
+	 * @param	{String}	parent		Parent type. Can be 'article', 'page', etc.
+	 * @param	{Element}	element 	The copied / moved element
+	 * @param	{Element}	droppable	DOM target
+	 * @param	{Event}		event		Event
 	 */
 	dropContentElement: function(parent, element, droppable, event)
 	{
@@ -1485,6 +1495,9 @@ ION.append({
 	 * Drop one Content Element in a page (in the tree)
 	 * Copy / Move depending on the SHIFT key
 	 *
+	 * @param	{Element}	element
+	 * @param	{Element}	droppable
+	 * @param	{Event}		event
 	 */
 	dropContentElementInPage: function(element, droppable, event)
 	{
@@ -1496,6 +1509,9 @@ ION.append({
 	 * Drop one Content Element in an article (in the tree)
 	 * Copy / Move depending on the SHIFT key
 	 *
+	 * @param	{Element}	element
+	 * @param	{Element}	droppable
+	 * @param	{Event}		event
 	 */
 	dropContentElementInArticle: function(element, droppable, event)
 	{
@@ -1507,6 +1523,9 @@ ION.append({
 	/**
 	 * Drops an article into a page (from tree to page)
 	 *
+	 * @param	{Element}	element
+	 * @param	{Element}	droppable
+	 * @param	{Event}		event
 	 */
 	dropArticleInPage: function(element, droppable, event)
 	{
@@ -1522,6 +1541,9 @@ ION.append({
 	/**
 	 * Drops a page into an article (from tree to article's parents)
 	 *
+	 * @param	{Element}	element
+	 * @param	{Element}	droppable
+	 * @param	{Event}		event
 	 */
 	dropPageInArticle: function(element, droppable, event)
 	{
@@ -1531,6 +1553,11 @@ ION.append({
 	},
 
 
+	/**
+	 * @param	{String}	link_type
+	 * @param	{Element}	element
+	 * @param	{Element}	droppable
+	 */
 	dropElementAsLink: function(link_type, element, droppable)
 	{
 		// Target link rel
@@ -1561,7 +1588,9 @@ ION.append({
 	/**
 	 * Drops one article as link for another article / page
 	 *
-	 *
+	 * @param	{Element}	element
+	 * @param	{Element}	droppable
+	 * @param	{Event}		event
 	 */
 	dropArticleAsLink: function(element, droppable, event)
 	{
@@ -1572,7 +1601,9 @@ ION.append({
 	/**
 	 * Drops one page as link for another article / page
 	 *
-	 *
+	 * @param	{Element}	element
+	 * @param	{Element}	droppable
+	 * @param	{Event}		event
 	 */
 	dropPageAsLink: function(element, droppable, event)
 	{
@@ -1689,8 +1720,8 @@ ION.append({
 		var sep = arguments[2];
 		if ( ! sep) sep = '-';
 
-		var src = $(src);
-		var target = $(target);
+		src = $(src);
+		target = $(target);
 		
 		if (src && target)
 		{
@@ -1712,7 +1743,7 @@ ION.append({
 		var sep = arguments[1];
 		if ( ! sep) sep = '-';
 
-		var text = text.toLowerCase();
+		text = text.toLowerCase();
 
 		/*
 		text = text.replace(/ /g, '-');

themes/admin/javascript/ionize/ionize_core.js
@@ -168,7 +168,7 @@ ION.append({
 		{
 			if ( ! self.isAssetPlanned(source))
 			{
-				sources.push(source)
+				sources.push(source);
 			}
 		});
 
@@ -328,6 +328,7 @@ ION.append({
 	/** 
 	 * Clears one form field
 	 *
+	 * @param	{String}	field
 	 */
 	clearField: function(field) 
 	{
@@ -338,12 +339,21 @@ ION.append({
 		}
 	},
 
+	/**
+	 * @param	{String} url
+	 * @returns {Boolean}
+	 */
 	checkUrl: function(url)
 	{
 		var RegexUrl = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/
 		return RegexUrl.test(url);
 	},
 
+	/**
+	 * @param	{String} selector
+	 * @param	{String} selectors
+	 * @param	{String} cl
+	 */
 	setSelected: function(selector, selectors, cl)
 	{
 		$$(selectors).each(function(el){
@@ -368,10 +378,12 @@ ION.append({
 	/**
 	 * Add one list of values to cookie
 	 *
+	 * @param {String} name
+	 * @param {String|number} value
 	 */
 	listAddToCookie: function(name, value)
 	{
-		var list = Array();
+		var list = [];
 		if (Cookie.read(name))
 			list = (Cookie.read(name)).split(',');
 		if (!list.contains(value))
@@ -385,10 +397,12 @@ ION.append({
 	/**
 	 * Remove one list of values from cookie
 	 *
+	 * @param	{String}		name
+	 * @param	{String|number} value
 	 */
 	listDelFromCookie: function(name, value)
 	{
-		var list = Array();
+		var list = [];
 		if (Cookie.read(name))
 			list = (Cookie.read(name)).split(',');
 		if (list.contains(value))
@@ -425,8 +439,7 @@ ION.append({
 			}
 		}
 
-		var options  =
-		{
+		return {
 			id: 'filemanagerWindow',
 			title: 'Media Manager',
 			container: document.body,
@@ -443,26 +456,20 @@ ION.append({
 				this.filemanager = null;
 			}
 		};
-
-		return options;
 	},
 
 	getWeekDayName: function(date)
 	{
-		if (typeOf(date) == 'date')
-			return Lang.get('day_' + date.getDay());
-		else
-			return '';
+		return (typeOf(date) == 'date')
+			? Lang.get('day_' + date.getDay())
+			: '';
 	},
 
 	getMonthName: function(date)
 	{
-		if (typeOf(date) == 'date')
-			return Lang.get('month_' + (date.getMonth() + 1));
-		else
-			return '';
-
-		return true;
+		return (typeOf(date) == 'date')
+			? Lang.get('month_' + (date.getMonth() + 1))
+			: '';
 	}
 });
 
@@ -484,7 +491,6 @@ Number.extend({
 	 * Returns a random number 
 	 * version: 1008.1718
 	 * discuss at: http://phpjs.org/functions/rand    // +   original by: Leslie Hoare
-	 *
 	 */
 	rand: function(min, max) {
 		var argc = arguments.length;
@@ -499,10 +505,8 @@ Number.extend({
 
 Number.extend({
 
-
 	round: function(num, dec) {
-		var result = Math.round( Math.round( num * Math.pow( 10, dec + 1 ) ) / Math.pow( 10, 1 ) ) / Math.pow(10,dec);
-		return result;
+		return Math.round( Math.round( num * Math.pow( 10, dec + 1 ) ) / Math.pow( 10, 1 ) ) / Math.pow(10,dec);
 	}
 
 });
@@ -530,7 +534,7 @@ Date.extend({
 			var t = d.split(/[- :]/);
 
 			// Apply each element to the Date function
-			var d = new Date(t[0], t[1]-1, t[2], t[3], t[4], t[5]);
+			d = new Date(t[0], t[1]-1, t[2], t[3], t[4], t[5]);
 
 			var h = d.getHours();
 			var m = d.getMonth();
@@ -596,15 +600,14 @@ Date.extend({
 
 	mysqlToUnix: function(date)
 	{
-		if (typeOf(date) != 'null')
-			return Math.floor(new Date(date.replace(' ', 'T')).getTime() / 1000);
-		else
-			return Math.floor(new Date('1970-01-01T01:00:00').getTime() / 1000);
+		return (typeOf(date) != 'null')
+			? Math.floor(new Date(date.replace(' ', 'T')).getTime() / 1000)
+			: Math.floor(new Date('1970-01-01T01:00:00').getTime() / 1000);
 	}
 });
 
 String.extend({
-	
+
 	htmlspecialchars_decode: function(text)
 	{
 		var tmp = new Element('span',{ 'html':text });
@@ -625,18 +628,15 @@ String.extend({
 			var specified = eval("["+arguments[2]+"]");
 			if(allowed)
 			{
-				var regex='</?(?!(' + specified.join('|') + '))\b[^>]*>';
-				html = html.replace(new RegExp(regex, 'gi'), '');
+				html = html.replace(new RegExp('</?(?!(' + specified.join('|') + '))\b[^>]*>', 'gi'), '');
 			}
 			else
 			{
-				var regex='</?(' + specified.join('|') + ')\b[^>]*>';
-				html = html.replace(new RegExp(regex, 'gi'), '');
+				html = html.replace(new RegExp('</?(' + specified.join('|') + ')\b[^>]*>', 'gi'), '');
 			}
 		}
 
-		var clean_string = html;
-		return clean_string;
+		return html;
 	},
 
 	capitalize: function(text)
@@ -728,10 +728,10 @@ Object.append(Element.NativeEvents, {
 		key_access: /^\.([a-z_][a-z_\d]*)/i,
 		index_access: /^\[(\d+)\]/,
 		sign: /^[\+\-]/
-	}
+	};
 
 	function sprintf() {
-		var key = arguments[0], cache = sprintf.cache
+		var key = arguments[0], cache = sprintf.cache;
 		if (!(cache[key] && cache.hasOwnProperty(key))) {
 			cache[key] = sprintf.parse(key)
 		}
@@ -741,14 +741,14 @@ Object.append(Element.NativeEvents, {
 	sprintf.format = function(parse_tree, argv) {
 		var cursor = 1, tree_length = parse_tree.length, node_type = "", arg, output = [], i, k, match, pad, pad_character, pad_length, is_positive = true, sign = ""
 		for (i = 0; i < tree_length; i++) {
-			node_type = get_type(parse_tree[i])
+			node_type = get_type(parse_tree[i]);
 			if (node_type === "string") {
 				output[output.length] = parse_tree[i]
 			}
 			else if (node_type === "array") {
-				match = parse_tree[i] // convenience purposes only
+				match = parse_tree[i]; // convenience purposes only
 				if (match[2]) { // keyword argument
-					arg = argv[cursor]
+					arg = argv[cursor];
 					for (k = 0; k < match[2].length; k++) {
 						if (!arg.hasOwnProperty(match[2][k])) {
 							throw new Error(sprintf("[sprintf] property '%s' does not exist", match[2][k]))
@@ -777,53 +777,61 @@ Object.append(Element.NativeEvents, {
 
 				switch (match[8]) {
 					case "b":
-						arg = arg.toString(2)
-						break
+						arg = arg.toString(2);
+						break;
+
 					case "c":
-						arg = String.fromCharCode(arg)
-						break
+						arg = String.fromCharCode(arg);
+						break;
+
 					case "d":
-						arg = parseInt(arg, 10)
-						break
+						arg = parseInt(arg, 10);
+						break;
+
 					case "e":
-						arg = match[7] ? arg.toExponential(match[7]) : arg.toExponential()
-						break
+						arg = match[7] ? arg.toExponential(match[7]) : arg.toExponential();
+						break;
+
 					case "f":
-						arg = match[7] ? parseFloat(arg).toFixed(match[7]) : parseFloat(arg)
-						break
+						arg = match[7] ? parseFloat(arg).toFixed(match[7]) : parseFloat(arg);
+						break;
+
 					case "o":
-						arg = arg.toString(8)
-						break
+						arg = arg.toString(8);
+						break;
+
 					case "s":
-						arg = ((arg = String(arg)) && match[7] ? arg.substring(0, match[7]) : arg)
-						break
+						arg = ((arg = String(arg)) && match[7] ? arg.substring(0, match[7]) : arg);
+						break;
+
 					case "u":
-						arg = arg >>> 0
-						break
+						arg = arg >>> 0;
+						break;
+
 					case "x":
-						arg = arg.toString(16)
-						break
+						arg = arg.toString(16);
+						break;
+
 					case "X":
-						arg = arg.toString(16).toUpperCase()
+						arg = arg.toString(16).toUpperCase();
 						break
 				}
 				if (!is_positive || (re.number.test(match[8]) && match[3])) {
-					sign = is_positive ? "+" : "-"
+					sign = is_positive ? "+" : "-";
 					arg = arg.toString().replace(re.sign, "")
 				}
-				pad_character = match[4] ? match[4] == "0" ? "0" : match[4].charAt(1) : " "
-				pad_length = match[6] - (sign + arg).length
-				pad = match[6] ? str_repeat(pad_character, pad_length) : ""
+				pad_character = match[4] ? match[4] == "0" ? "0" : match[4].charAt(1) : " ";
+				pad_length = match[6] - (sign + arg).length;
+				pad = match[6] ? str_repeat(pad_character, pad_length) : "";
 				output[output.length] = match[5] ? sign + arg + pad : (pad_character == 0 ? sign + pad + arg : pad + sign + arg)
 			}
 		}
 		return output.join("")
-	}
+	};
 
-	sprintf.cache = {}
-
+	sprintf.cache = {};
 	sprintf.parse = function(fmt) {
-		var _fmt = fmt, match = [], parse_tree = [], arg_names = 0
+		var _fmt = fmt, match = [], parse_tree = [], arg_names = 0;
 		while (_fmt) {
 			if ((match = re.text.exec(_fmt)) !== null) {
 				parse_tree[parse_tree.length] = match[0]
@@ -833,70 +841,70 @@ Object.append(Element.NativeEvents, {
 			}
 			else if ((match = re.placeholder.exec(_fmt)) !== null) {
 				if (match[2]) {
-					arg_names |= 1
-					var field_list = [], replacement_field = match[2], field_match = []
+					arg_names |= 1;
+					var field_list = [], replacement_field = match[2], field_match = [];
 					if ((field_match = re.key.exec(replacement_field)) !== null) {
-						field_list[field_list.length] = field_match[1]
+						field_list[field_list.length] = field_match[1];
 						while ((replacement_field = replacement_field.substring(field_match[0].length)) !== "") {
 							if ((field_match = re.key_access.exec(replacement_field)) !== null) {
-								field_list[field_list.length] = field_match[1]
+								field_list[field_list.length] = field_match[1];
 							}
 							else if ((field_match = re.index_access.exec(replacement_field)) !== null) {
-								field_list[field_list.length] = field_match[1]
+								field_list[field_list.length] = field_match[1];
 							}
 							else {
-								throw new SyntaxError("[sprintf] failed to parse named argument key")
+								throw new SyntaxError("[sprintf] failed to parse named argument key");
 							}
 						}
 					}
 					else {
-						throw new SyntaxError("[sprintf] failed to parse named argument key")
+						throw new SyntaxError("[sprintf] failed to parse named argument key");
 					}
-					match[2] = field_list
+					match[2] = field_list;
 				}
 				else {
 					arg_names |= 2
 				}
 				if (arg_names === 3) {
-					throw new Error("[sprintf] mixing positional and named placeholders is not (yet) supported")
+					throw new Error("[sprintf] mixing positional and named placeholders is not (yet) supported");
 				}
 				parse_tree[parse_tree.length] = match
 			}
 			else {
-				throw new SyntaxError("[sprintf] unexpected placeholder")
+				throw new SyntaxError("[sprintf] unexpected placeholder");
 			}
-			_fmt = _fmt.substring(match[0].length)
+			_fmt = _fmt.substring(match[0].length);
 		}
 		return parse_tree
-	}
+	};
 
 	var vsprintf = function(fmt, argv, _argv) {
-		_argv = (argv || []).slice(0)
-		_argv.splice(0, 0, fmt)
-		return sprintf.apply(null, _argv)
-	}
+		_argv = (argv || []).slice(0);
+		_argv.splice(0, 0, fmt);
+		return sprintf.apply(null, _argv);
+	};
 
 	/**
 	 * helpers
 	 */
 	function get_type(variable) {
-		return Object.prototype.toString.call(variable).slice(8, -1).toLowerCase()
+		return Object.prototype.toString.call(variable).slice(8, -1).toLowerCase();
 	}
 
 	function str_repeat(input, multiplier) {
-		return Array(multiplier + 1).join(input)
+		return Array(multiplier + 1).join(input);
 	}
 
 	/**
 	 * export to either browser or node.js
 	 */
 	if (typeof exports !== "undefined") {
-		exports.sprintf = sprintf
-		exports.vsprintf = vsprintf
+		exports.sprintf = sprintf;
+		exports.vsprintf = vsprintf;
 	}
 	else {
-		window.sprintf = sprintf
-		window.vsprintf = vsprintf
+		window.sprintf = sprintf;
+		window.vsprintf = vsprintf;
 
 		if (typeof define === "function" && define.amd) {
 			define(function() {
@@ -907,7 +915,7 @@ Object.append(Element.NativeEvents, {
 			})
 		}
 	}
-})(typeof window === "undefined" ? this : window)
+})(typeof window === "undefined" ? this : window);
 
 
 String.prototype.trimLeft = function(charlist) {

themes/admin/javascript/ionize/ionize_droppable.js
@@ -12,7 +12,7 @@ ION.Droppable = new Class({
 	{
 		this.setOptions(options);
 		
-		var options = this.options;
+		options = this.options;
 		
 		/* Add focus in/out and blur events only if input has not the ".nofocus" class
 		 *

themes/admin/javascript/ionize/ionize_extendmanager.js
@@ -12,7 +12,7 @@ ION.ExtendManager = new Class({
 
 	/**
 	 *
-	 * @param options
+	 * @param	{Object}	options
 	 */
 	initialize: function()
 	{
@@ -42,7 +42,7 @@ ION.ExtendManager = new Class({
 		this.w =            null;       // window
 		this.wContainer =   null;       // Items container (in window)
 		this.destination =  null;       // Destination container (ID) : TabSwapper only for the moment
-		this.destinationTitle = null    // Destination container title : Tab title only for the moment
+		this.destinationTitle = null;   // Destination container title : Tab title only for the moment
 
 		// Data posted by getWindowExtendListContent()
 		this.post = null;
@@ -71,12 +71,12 @@ ION.ExtendManager = new Class({
 	/**
 	 * Initialize basis properties
 	 *
-	 * @param options
+	 * @param	{Object}	options
 	 */
-	init: function(opt)
+	init: function(options)
 	{
 		var self = this,
-			opt = typeOf(opt) != 'null' ? opt : {};
+			opt = typeOf(options) != 'null' ? options : {};
 
 		this.post = null;
 		if (opt.context) this.context = opt.context;
@@ -108,7 +108,7 @@ ION.ExtendManager = new Class({
 	/**
 	 * Opens New Extend Window
 	 *
-	 * @param Object 	{
+	 * @param {Object} 	{
 	 * 						parent: 'contact',
 	 * 						id_parent: null,
 	 * 						onSuccess: function(json, this){}
@@ -172,7 +172,7 @@ ION.ExtendManager = new Class({
 	 * Extend Definition Edition
 	 * Opens Extend field definition Editor window
 	 *
-	 * @param id
+	 * @param	{String}	id
 	 */
 	editExtend: function(id)
 	{
@@ -212,6 +212,9 @@ ION.ExtendManager = new Class({
 		);
 	},
 
+	/**
+	 * @param	{Object}	extend
+	 */
 	delete: function(extend)
 	{
 		var options = typeOf(arguments[1]) != 'null' ? arguments[1] : {};
@@ -753,10 +756,7 @@ ION.ExtendManager = new Class({
 	 * linked to one context
 	 * and to one parent
 	 *
-	 * @param options	if 'onSucess' is set in options, this method will be called.
-	 * 					{
-	 * 						onSuccess: function(extends)
-	 * 					}
+	 * @param	{Object}	[options]	{ onSuccess: function(extends) }	if 'onSuccess' is set in options, this method will be called.
 	 */
 	getContextInstances: function()
 	{
@@ -793,12 +793,9 @@ ION.ExtendManager = new Class({
 
 
 	/**
-	 * Get Extend fileds definitions for one parent type.
+	 * Get Extend field definitions for one parent type.
 	 *
-	 * @param options	if 'onSucess' is set in options, this method will be called.
-	 * 					{
-	 * 						onSuccess: function(extends)
-	 * 					}
+	 * @param	{Object}	[options]		{ onSuccess: function(extends) }	if 'onSuccess' is set in options, this method will be called.
 	 */
 	getParentDefinitions: function()
 	{

commit b2bbdd7aa2cbf9facef28806887fee2558033fb4

Fix bug when showing permissions in backend
The lock symbols in the menu are now displayed again.
This bug has been introduced in 7c9735f5f086fc5284a17307b0a18110e491dcf7.

themes/admin/javascript/ionize/ionize_authority.js
@@ -138,14 +138,17 @@ ION.Authority = new Class({
 
 	resourceHasRule: function(resource)
 	{
+		var has = false;
+
 		Object.each(ION.Authority.all_rules, function(rule)
 		{
 			if (resource == rule.resource)
 			{
-				return true;
+				has = true;
+				return;
 			}
 		});
 
-		return false;
+		return has;
 	}
 });