e54e9aacdafe9180557972097354f8307be25d95
Kay Stenschke <info@stenschke.com>
2015-08-16 22:51:20 +0200

    Added element usage deletion confirmation text

--- application/language/en/admin_lang.php
+++ application/language/en/admin_lang.php
@@ -393,6 +393,7 @@ $lang['ionize_button_yes'] = 'Yes';
 $lang['button_delete_installer_done_admin'] = 'Done! Go to Admin panel';
 $lang['button_delete_installer_done_site'] = 'Done! Go to website';
 $lang['ionize_confirm_element_delete'] = 'Do you want to definitely delete this element?';
+$lang['ionize_confirm_element_delete_usage'] = 'Do you want to definitely delete this element usage?';
 $lang['ionize_message_delete_installer'] = 'IMPORTANT: <br/>For security reasons, please delete the <b>"/install"</b> folder. Ionize will not be available until this folder is deleted.';
 $lang['ionize_message_element_not_found'] = 'This element wasn\'t found!';
 $lang['ionize_message_form_validation_error'] = 'Some data are missing or incorrect';
