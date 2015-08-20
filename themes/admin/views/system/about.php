34e26bb5e7ee647fdf1c39e648e6d1f1d5e721e5
Kay Stenschke <info@stenschke.com>
2015-08-20 10:17:40 +0200

    Update about.php

--- themes/admin/views/system/about.php
+++ themes/admin/views/system/about.php
@@ -1,14 +1,14 @@
 <p class="version"><?php echo($this->config->item('version')) ;?></p>
 
-<p><strong><?php echo lang('ionize_label_about_us_author'); ?> : </strong> <a href="http://www.ionizecms.com/">Ionize Dev Team</a></p>
+<p><strong><?php echo lang('ionize_label_about_us_author'); ?>: </strong> <a href="http://www.ionizecms.com/">Ionize Dev Team</a></p>
 
-<p><strong><?php echo lang('ionize_label_about_us_contributors'); ?> : </strong>
+<p><strong><?php echo lang('ionize_label_about_us_contributors'); ?>: </strong>
 	<a href="http://www.partikule.net" target="_blank">Michel-Ange Kuntz</a>,
 	<a href="http://www.toopixel.ch" target="_blank">Christophe Prudent</a>,
     <a href="http://www.altivebir.com.tr" target="_blank">İskender Totoğlu</a>,
     <a href="http://www.coexec.com" target="_blank">Kay Stenschke</a>,
 </p>
 
-<p><strong><?php echo lang('ionize_label_about_us_thanks_to'); ?> : </strong> <a href="http://ellislab.com/">EllisLab</a>, <a href="http://greghoustondesign.com/">Greg Houston</a>
+<p><strong><?php echo lang('ionize_label_about_us_thanks_to'); ?>: </strong> <a href="http://ellislab.com/">EllisLab</a>, <a href="http://greghoustondesign.com/">Greg Houston</a>
 
-<p><strong>License : </strong> MIT License</p>
+<p><strong>License: </strong> MIT License</p>
