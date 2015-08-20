86d689fa1c54ed6a8a4a8f51d4abbb6f1c990d14
Kay Stenschke <info@stenschke.com>
2015-08-20 10:18:59 +0200

    Update delete_installer.php

--- themes/admin/views/system/delete_installer.php
+++ themes/admin/views/system/delete_installer.php
@@ -5,31 +5,20 @@
 <title><?php echo lang('title_ionize_installation'); ?></title>
 
 <link rel="stylesheet" href="<?php echo base_url(); ?>install/assets/css/installer.css" type="text/css" />
-
 </head>
-
 <body>
-
 <div id="page">
-
 	<div id="content">
 		<div class="block">
-
 			<img src="<?php echo base_url(); ?>install/assets/images/ionize_logo_install.png" />
-
 			<h1><?php echo lang('title_delete_installer'); ?></h1>
-
 			<p><?php echo lang('ionize_message_delete_installer'); ?></p>
-
 			<div class="buttons">
 				<button type="button" class="button yes left" onclick="location.href='<?php echo admin_url(); ?>';"><?php echo lang('button_delete_installer_done_admin'); ?></button>
 				<button type="button" class="button yes right" onclick="location.href='<?php echo base_url(); ?>';"><?php echo lang('button_delete_installer_done_site'); ?></button>
 			</div>
-
 		</div>
 	</div>
-
 </div>
-
 </body>
 </html>
