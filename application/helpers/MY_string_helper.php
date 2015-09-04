52dcfc007337e00c5eed47d4b682c6f9f1f64a18
Kay Stenschke <info@stenschke.com>
2015-09-04 14:12:50 +0200

    Added string helper: replace_first, replace_last

--- application/helpers/MY_string_helper.php
+++ application/helpers/MY_string_helper.php
@@ -89,5 +89,66 @@ if ( ! function_exists('strip_html'))
 }
 
 
+// ------------------------------------------------------------------------
+
+
+/*
+ * Replace first occurrence of substring
+ *
+ * @access	public
+ */
+if ( ! function_exists('replace_first')) {
+
+	/**
+	 * @param	String	$search		string to be replaced
+	 * @param	String	$replace	string to used for replacement
+	 * @param	String	$string		the source string to do the replacement inside
+	 * @return	String	The string with the replacement done
+	 */
+	function replace_first($string, $search, $replace = '')
+	{
+		if( strlen( $search ) > 0 ) {
+			$pos = strpos( $string, $search );
+			if( is_int( $pos ) ) {
+				$len = strlen( $search );
+
+				return substr_replace( $string, $replace, $pos, $len );
+			}
+		}
+
+		return $string;
+	}
+}
+
+
+// ------------------------------------------------------------------------
+
+
+/*
+ * Replace last occurrence of substring
+ *
+ * @access	public
+ */
+if ( ! function_exists('replace_last')) {
+
+	/**
+	 * @param	String	$search
+	 * @param	String	$replace
+	 * @param	String	$subject
+	 * @return	String
+	 */
+	function replace_last($search, $replace, $subject)
+	{
+		$pos = strrpos($subject, $search);
+
+		if ($pos !== false) {
+			$subject = substr_replace($subject, $replace, $pos, strlen($search));
+		}
+
+		return $subject;
+	}
+}
+
+
 /* End of file MY_string_helper.php */
 /* Location: ./application/helpers/MY_string_helper.php */
